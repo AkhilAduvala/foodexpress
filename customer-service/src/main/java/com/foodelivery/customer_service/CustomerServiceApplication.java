d37a9d9f1b619877cdb229ff24d6b825b91e0938
AKHIL ADUVALA <aduvalaakhil@gmail.com>
2025-03-15 23:34:03 +0530

    cache for customer service
    adding a in-memory cache for customer service apis

--- customer-service/src/main/java/com/foodelivery/customer_service/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/foodelivery/customer_service/CustomerServiceApplication.java
@@ -2,9 +2,11 @@ package com.foodelivery.customer_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient   // Registers this microservice with Eureka
+@EnableDiscoveryClient
+@EnableCaching// Registers this microservice with Eureka
 @SpringBootApplication
 public class CustomerServiceApplication {
 
--- customer-service/src/main/java/com/foodelivery/customer_service/service/CustomerService.java
+++ customer-service/src/main/java/com/foodelivery/customer_service/service/CustomerService.java
@@ -4,6 +4,9 @@ import com.foodelivery.customer_service.dto.CustomerDto;
 import com.foodelivery.customer_service.exception.CustomerNotFoundException;
 import com.foodelivery.customer_service.model.Customer;
 import com.foodelivery.customer_service.repo.CustomerRepo;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -20,6 +23,7 @@ public class CustomerService {
         this.customerRepo = customerRepo;
     }
 
+    @Cacheable(value = "allcustomers", key = "#customerId")
     public ResponseEntity<List<CustomerDto>> getAllCustomers(){
         List<CustomerDto> customersDto = new ArrayList<>();
         List<Customer> customers = customerRepo.findAll();
@@ -37,6 +41,7 @@ public class CustomerService {
         return new ResponseEntity<>(customersDto, HttpStatus.OK);
     }
 
+    @Cacheable(value = "customer", key = "#customerId")
     public ResponseEntity<CustomerDto> getCustomer(int customerId){
         CustomerDto customerDto = new CustomerDto();
 
@@ -65,6 +70,7 @@ public class CustomerService {
         return new ResponseEntity<>(customerDto, HttpStatus.CREATED);
     }
 
+    @CachePut(value = "allcustomers", key = "#customerId")
     public ResponseEntity<CustomerDto> updateCustomer(CustomerDto  customerDto){
         Customer customer = new Customer();
 
@@ -81,12 +87,13 @@ public class CustomerService {
         return new ResponseEntity<CustomerDto>(customerDto, HttpStatus.OK);
     }
 
+    @CacheEvict(value = "allcustomers", key = "#customerId")
     public ResponseEntity<Void> deleteCustomer(int customerId){
         Customer customer = customerRepo.findById(customerId)
                 .orElseThrow(() -> new CustomerNotFoundException("Unable to find the customer with Id : "
                         + customerId));
 
-        customerRepo.deleteById(customerId);
+        customerRepo.deleteById(customer.getCustomer_Id());
         return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
 
     }
